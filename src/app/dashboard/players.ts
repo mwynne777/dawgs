1081f04f9abe1b8a0f9e6e4633e4f6791cdca7c2
Michael Wynne <michael.wynne@uconn.edu>
2025-01-05 22:06:09 -0500

    fix team association typo

--- src/app/dashboard/players.ts
+++ src/app/dashboard/players.ts
@@ -15,7 +15,7 @@ export const PLAYERS: Player[] = [
     { id: "4433083", name: "Cam Spencer", teamId: 29, leagueId: 46 },
     { id: "4683750", name: "Jordan Hawkins", teamId: 3, leagueId: 46 },
     { id: "4845367", name: "Stephon Castle", teamId: 24, leagueId: 46 },
-    { id: "4397179", name: "Tyrese Martin", teamId: 24, leagueId: 46 },
+    { id: "4397179", name: "Tyrese Martin", teamId: 17, leagueId: 46 },
     // { id: "4592965", name: "Tristen Newton", teamId: 7, leagueId: 69 },
   ] as const;
 
