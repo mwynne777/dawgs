2345cfa172f691c6269b54353a5974d9adf5deeb
Michael Wynne <michael.wynne@uconn.edu>
2025-01-07 21:11:12 -0500

    reorder stats in recent game card and hide some on mobile

--- src/app/where-are-they-now/[collegeId]/recent-game-card.tsx
+++ src/app/where-are-they-now/[collegeId]/recent-game-card.tsx
@@ -1,32 +1,60 @@
 const GameCard = ({ stats }: { stats: string[] }) => {
+  const [
+    min,
+    fg,
+    threePt,
+    _ft,
+    _ored,
+    _dred,
+    reb,
+    ast,
+    stl,
+    blk,
+    to,
+    _pf,
+    plusMinus,
+    pts,
+  ] = stats;
   return (
     <table className="min-w-full border-collapse border border-gray-200">
       <thead>
         <tr className="bg-gray-100">
           <th className="border border-gray-200 px-4 py-2">MIN</th>
-          <th className="border border-gray-200 px-4 py-2">FG</th>
-          <th className="border border-gray-200 px-4 py-2">3PT</th>
+          <th className="border border-gray-200 px-4 py-2">PTS</th>
           <th className="border border-gray-200 px-4 py-2">REB</th>
           <th className="border border-gray-200 px-4 py-2">AST</th>
           <th className="border border-gray-200 px-4 py-2">STL</th>
           <th className="border border-gray-200 px-4 py-2">BLK</th>
-          <th className="border border-gray-200 px-4 py-2">TO</th>
+          <th className="hidden border border-gray-200 px-4 py-2 sm:table-cell">
+            FG
+          </th>
+          <th className="hidden border border-gray-200 px-4 py-2 sm:table-cell">
+            3PT
+          </th>
+          <th className="hidden border border-gray-200 px-4 py-2 sm:table-cell">
+            TO
+          </th>
           <th className="border border-gray-200 px-4 py-2">+/-</th>
-          <th className="border border-gray-200 px-4 py-2">PTS</th>
         </tr>
       </thead>
       <tbody>
         <tr>
-          <td className="border border-gray-200 px-4 py-2">{stats[0]}</td>
-          <td className="border border-gray-200 px-4 py-2">{stats[1]}</td>
-          <td className="border border-gray-200 px-4 py-2">{stats[3]}</td>
-          <td className="border border-gray-200 px-4 py-2">{stats[6]}</td>
-          <td className="border border-gray-200 px-4 py-2">{stats[7]}</td>
-          <td className="border border-gray-200 px-4 py-2">{stats[8]}</td>
-          <td className="border border-gray-200 px-4 py-2">{stats[9]}</td>
-          <td className="border border-gray-200 px-4 py-2">{stats[10]}</td>
-          <td className="border border-gray-200 px-4 py-2">{stats[12]}</td>
-          <td className="border border-gray-200 px-4 py-2">{stats[13]}</td>
+          <td className="border border-gray-200 px-4 py-2">{min}</td>
+          <td className="border border-gray-200 px-4 py-2">{pts}</td>
+          <td className="border border-gray-200 px-4 py-2">{reb}</td>
+          <td className="border border-gray-200 px-4 py-2">{ast}</td>
+          <td className="border border-gray-200 px-4 py-2">{stl}</td>
+          <td className="border border-gray-200 px-4 py-2">{blk}</td>
+          <td className="hidden border border-gray-200 px-4 py-2 sm:table-cell">
+            {fg}
+          </td>
+          <td className="hidden border border-gray-200 px-4 py-2 sm:table-cell">
+            {threePt}
+          </td>
+          <td className="hidden border border-gray-200 px-4 py-2 sm:table-cell">
+            {to}
+          </td>
+          <td className="border border-gray-200 px-4 py-2">{plusMinus}</td>
         </tr>
       </tbody>
     </table>
