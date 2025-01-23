c499cebe1fffbc615d7e5b1746686c7915b80c57
Michael Wynne <michael.wynne@uconn.edu>
2025-01-22 21:32:17 -0500

    more cleanup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,5 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import { Player, PLAYERS } from "./players";
-import { leagues } from "./leagues";
-import { Game } from "./teams";
-import playerService from "./services/player-service";
 import playerStatsService from "./services/player-stats-service";
 
 dotenv.config();
@@ -11,62 +7,8 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-const getBoxScoreUrl = (leagueAbbreviation: string, gameId: string) => {
-  return `${process.env.STATS_API_BASE_URL}${leagueAbbreviation}/summary?region=us&lang=en&contentorigin=espn&event=${gameId}`;
-}
-
-export type GameRecord = {
-  game: Game;
-  players: { stats: string[]; player: Player }[];
-};
-
-type BoxScoreStatistic = {
-  athletes: { athlete: { id: string }; stats: string[] }[]
-}
-
-type BoxscoreResponse = {
-  boxscore: {
-      players: {
-          statistics: BoxScoreStatistic[]
-      }[]
-  }
-}
-
-const getPlayers = async (id: string, league_id: number) => {
-    const players: { stats: string[]; player: Player }[] = [];
-    const leagueAbbreviation = Object.entries(leagues).find(([_, value]) => value.id === league_id)?.[0];
-    if(!leagueAbbreviation) return [];
-    const boxScoreResponse = await fetch(getBoxScoreUrl(leagueAbbreviation, id));
-    const boxScoreResponseParsed = await boxScoreResponse.json() as BoxscoreResponse;
-
-    if (!("players" in boxScoreResponseParsed.boxscore)) return [];
-
-    boxScoreResponseParsed.boxscore.players.forEach((element) => {
-        const athletes = element.statistics[0]!.athletes;
-        const uconnPlayerIds = Object.values(PLAYERS).map((p) => p.id);
-        const athletesOfInterest = athletes.filter(
-            (a: { athlete: { id: string } }) =>
-            uconnPlayerIds.includes(a.athlete.id),
-        );
-        athletesOfInterest.forEach(
-            (a: { athlete: { id: string }; stats: string[] }) => {
-            players.push({
-                stats: a.stats,
-                player: PLAYERS.find((p) => p.id === a.athlete.id)!,
-            });
-        },
-    );
-  });
-  return players;
-}
-
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
-});
-
-app.get("/players", async (req: Request, res: Response) => {
-  const result = await getPlayers(req.query.id as string, parseInt(req.query.league_id as string));
-  res.status(200).json(result);
 });
 
 app.get("/seed/player-stats", async (req: Request, res: Response) => {
--- server/src/leagues.ts
+++
@@ -1,7 +0,0 @@
-export type LeagueAbbreviation = 'nba' | 'nba-development' /*| 'wnba'*/
-
-export const leagues: Record<LeagueAbbreviation, {id: number, displayName: string}> = {
-    nba: {id: 46, displayName: 'NBA'},
-    'nba-development': {id: 69, displayName: 'NBA G League'},
-    // wnba: {id: 59, displayName: 'WNBA'}
-} as const;
