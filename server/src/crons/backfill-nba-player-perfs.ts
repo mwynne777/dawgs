6f7452bacee1325fc910137f770f51732a87196b
Michael Wynne <michael.wynne@uconn.edu>
2025-01-26 16:37:19 -0500

    build some recursive logic and logs into cron job

--- server/src/crons/backfill-nba-player-perfs.ts
+++ server/src/crons/backfill-nba-player-perfs.ts
@@ -4,8 +4,18 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const main = async (rangeStart: number) => {
+  if (rangeStart > 1000) {
+    console.log("rangeStart is greater than 1000, stopping");
+    return;
+  }
+
   const result = await playerStatsService.getPlayerStats(rangeStart);
-  console.log(result);
+  console.log('rangeStart', rangeStart, result);
+
+  if (result.existingCount < 100) {
+    console.log(`existingCount: ${result.existingCount} is less than 100, continuing with rangeStart ${rangeStart + 100}`);
+    main(rangeStart + 100);
+  }
 };
 
 main(0);
