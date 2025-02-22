74465f788131d83fa8b52280767359299d01d602
Michael Wynne <michael.wynne@uconn.edu>
2025-02-21 22:37:13 -0500

    remove unnecessary wrapping div styles

--- ui/src/app/where-are-they-now/[collegeCode]/page.tsx
+++ ui/src/app/where-are-they-now/[collegeCode]/page.tsx
@@ -7,11 +7,11 @@ const WhereAreTheyNowPage = async ({
   params: Promise<{ collegeCode: string }>;
 }) => {
   return (
-    <div className="mx-auto max-w-[1000px] px-8">
+    <>
       <SuspensefulServerComponent date={new Date().toISOString()}>
         <Loader params={params} />
       </SuspensefulServerComponent>
-    </div>
+    </>
   );
 };
 
