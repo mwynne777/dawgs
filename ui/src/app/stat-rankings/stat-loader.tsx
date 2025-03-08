fe2eb4c5fd2943a2cf48c02d7200238d4d054237
Michael Wynne <michael.wynne@uconn.edu>
2025-03-07 22:54:47 -0500

    fix trend bug

--- ui/src/app/stat-rankings/stat-loader.tsx
+++ ui/src/app/stat-rankings/stat-loader.tsx
@@ -2,9 +2,8 @@ import collegesService from "../(services)/colleges-service";
 import StatRankingsClientComponent from "./client-component";
 
 const StatLoader = async ({}) => {
-  const year = 2025;
   const [collegeStatTotals, colleges, collegeSalaryTotals] = await Promise.all([
-    collegesService.getCollegeStatTotals(year),
+    collegesService.getCollegeStatTotals(),
     collegesService.getColleges(),
     collegesService.getCollegeSalaryTotals(),
   ]);
